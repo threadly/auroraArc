c92d461d5ea13f252ca0c0887f9168398e5fc596
Alessio Pollero <alessio.pollero@mycomp.it>
2018-01-05 12:25:13 +0100

    Fix compatibility issues with Java9

--- src/main/java/org/threadly/db/aurora/NonRegisteringDriver.java
+++ src/main/java/org/threadly/db/aurora/NonRegisteringDriver.java
@@ -10,17 +10,16 @@ import java.util.logging.Logger;
 import org.threadly.db.AbstractArcDriver;
 
 /**
- * Threadly's AuroraArc NonRegisteringDriver.  This Driver will create multiple connections for each returned 
- * connection it provides.  Using these connections to monitor the aurora state, and to distribute 
- * queries to multiple aurora servers when possible.
+ * Threadly's AuroraArc NonRegisteringDriver. This Driver will create multiple connections for each returned connection
+ * it provides. Using these connections to monitor the aurora state, and to distribute queries to multiple aurora
+ * servers when possible.
  * <p>
- * This is different from most SQL drivers, in that there is shared state between all returned 
- * connections.  This shared state allows things like fail conditions to be communicated quickly, 
- * allowing for intelligence of how to mitigate problems, and use potential secondary / slave 
- * servers as soon as possible.
+ * This is different from most SQL drivers, in that there is shared state between all returned connections. This shared
+ * state allows things like fail conditions to be communicated quickly, allowing for intelligence of how to mitigate
+ * problems, and use potential secondary / slave servers as soon as possible.
  * <p>
- * In general the user does not need to concern themselves with this benefit.  Just be aware that 
- * multiple connections will be established for every connection returned by this Driver.
+ * In general the user does not need to concern themselves with this benefit. Just be aware that multiple connections
+ * will be established for every connection returned by this Driver.
  * <p>
  * Possible URL configuration options:
  * <ul>
@@ -28,15 +27,15 @@ import org.threadly.db.AbstractArcDriver;
  * </ul>
  */
 public class NonRegisteringDriver extends AbstractArcDriver {
-
+  
   /**
-   * Another way to register the driver.  This is more convenient than `Class.forName(String)` as 
-   * no exceptions need to be handled (instead just relying on the compile time dependency).
+   * Another way to register the driver. This is more convenient than `Class.forName(String)` as no exceptions need to
+   * be handled (instead just relying on the compile time dependency).
    */
   public static void registerDriver() {
     // Nothing needed, just a nicer way to initialize the static registration compared to Class.forName.
   }
-
+  
   @Override
   public Connection connect(String url, Properties info) throws SQLException {
     if (DelegatingAuroraConnection.acceptsURL(url)) {
@@ -46,23 +45,23 @@ public class NonRegisteringDriver extends AbstractArcDriver {
       return null;
     }
   }
-
+  
   @Override
   public boolean acceptsURL(String url) {
     return DelegatingAuroraConnection.acceptsURL(url);
   }
-
+  
   @Override
   public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
     return DelegateDriver.getDriver().getPropertyInfo(url, info);
   }
-
+  
   @Override
   public boolean jdbcCompliant() {
     // should be compliant since just depending on mysql connector
     return DelegateDriver.getDriver().jdbcCompliant();
   }
-
+  
   @Override
   public Logger getParentLogger() throws SQLFeatureNotSupportedException {
     return DelegateDriver.getDriver().getParentLogger();
