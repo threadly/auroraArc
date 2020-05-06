75739982a0b563d9746c2371786d2b7c95c8776d
Mike Jensen <jent@threadly.org>
2020-05-05 18:13:02 -0600

    Revert "AuroraClusterMonitor: Add the ability to get notified of cluster state changes"
    This reverts commit 25dd490130d034c7ecb0330d3784addfdb47e78f.
    This does not seem useful currently, so we are removing to avoid the API sprawl.

--- arcCommon/src/test/java/org/threadly/db/aurora/AuroraClusterMonitorClusterCheckerTest.java
+++ arcCommon/src/test/java/org/threadly/db/aurora/AuroraClusterMonitorClusterCheckerTest.java
@@ -14,7 +14,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.threadly.concurrent.ReschedulingOperation;
 import org.threadly.concurrent.SubmitterScheduler;
-import org.threadly.db.aurora.AuroraClusterMonitor.AuroraClusterStateListener;
 import org.threadly.db.aurora.AuroraClusterMonitor.ClusterChecker;
 import org.threadly.db.aurora.AuroraClusterMonitor.ServerMonitor;
 import org.threadly.test.concurrent.TestableScheduler;
@@ -140,18 +139,6 @@ public class AuroraClusterMonitorClusterCheckerTest {
 
     for (ServerMonitor sm : clusterServers.values()) {
       assertFalse(sm.isHealthy()); // NPE should have made unhealthy
-    }
-  }
-  
-  @Test
-  public void listenerStateInitializedOnNextCheckTest() {
-    AuroraClusterStateListener mockListener = mock(AuroraClusterStateListener.class);
-    
-    clusterChecker.setClusterStateListener(mockListener);
-    assertEquals(1, testScheduler.advance(500));
-    
-    for (AuroraServer as : clusterServers.keySet()) {
-      verify(mockListener, times(1)).newReplica(eq(as));
     }
   }
   
